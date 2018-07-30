772cfc73d9e2a7ec6ae7cf87aa7e93851a1d2c91
Nzingani <isabelnzingani@outlook.com>
2018-07-30 13:48:59 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>P4ex7</title>
+  </head>
+  <body>
+    <p>
+  <?php
+function result($numb1 = 10,$numb2 = 15,$numb3 = 23){
+  return $numb1 + $numb2 +$numb3;
+}
+    echo result(10,15,23);
+  ?>
+  </p>
+</body>
